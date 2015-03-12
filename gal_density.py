6e7016aeee9fc8787f7275e4d26678319678efd2
asvath <ashaasvathaman@gmail.com>
2015-03-11 19:15:29 -0700

    changes

--- gal_density.py
+++ gal_density.py
@@ -1,11 +1,16 @@
 #calculates the density of a galaxy, where r is the half light radius in kpc
 import numpy as np
-def gal_density(sigma,r)
+def gal_density(sigma,r):
 	G=6.6784*10**-11
 	kpc=3.0856*10**19
 	radius=r*kpc
-	denisty=(9*sigma**2)/(20*np.pi*radius*G)
+	density=(9*sigma**2)/(20*np.pi*radius*G)
 	print density
 	return density
 
+
+
+if __name__ == "__main__":
+    import sys
+    gal_density(float(sys.argv[1]),float(sys.argv[2]))
 
