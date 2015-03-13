87b3d7ec9970a662f0cc21856ed9bcca54d4ac2c
asvath <ashaasvathaman@gmail.com>
2015-03-12 18:29:54 -0700

    changes

--- gal_density.py
+++ gal_density.py
@@ -1,12 +1,22 @@
 #calculates the density of a galaxy, where r is the half light radius in kpc
 import numpy as np
 def gal_density(sigma,r):
-	G=6.6784*10**-11
+	G=6.6784*10**(-11)
 	kpc=3.0856*10**19
 	radius=r*kpc
-	density=(9*sigma**2)/(20*np.pi*radius*G)
-	print density
-	return density
+	sigma=sigma*1000
+	density=(5*sigma**2)/(4*np.pi*(radius**2)*G)
+	#density=(3*sigma**2)/(4*np.pi*(radius**2)*G)
+	#print density
+	sun=density/(1.98*10**30)
+	mm=3.24*10**(-17)
+	den=sun/(mm**3)
+	print sigma
+	print den
+
+	#return density
+
+
 
 
 
--- x.py
+++ x.py
@@ -1,10 +1,11 @@
 #finds the X!
 import numpy as np
 def xval(density,mass,radius,delta):
+	denisty=density*10**-20
 	G=6.6784*10**-11
 	kpc=3.0856*10**19
 	r=radius*kpc
-	sun=1.989*10**30
+	sun=(1.989*10**30)*10**9
 	m=mass*sun
 	glob=4*np.pi*G*density*(1+delta)
 	glob2=(G*m*delta)/r**2
